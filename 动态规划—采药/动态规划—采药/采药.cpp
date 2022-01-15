0008a392089de43cf47cc3430bdefeefdf7c36d3
Donnykk <97417717+Donnykk@users.noreply.github.com>
2022-01-15 20:18:09 +0800

    Add files via upload

---
+++ 动态规划—采药/动态规划—采药/采药.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int arr[100][2];
+int main()
+{
+	int T, M; 
+	cin >> T >> M;
+	for (int i = 0; i < M; i++)
+		cin >> arr[i][0] >> arr[i][1];
+		cout << "Fuck";
+	system("pause");
+	return 0;
+}
